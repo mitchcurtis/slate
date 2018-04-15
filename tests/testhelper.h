36b6dcbaae80b0062e708dca03d0962b6b806b2d
Mitch Curtis <mitch.curtis@qt.io>
2018-04-15 22:28:54 +0200

    Add test for unimplemented auto export layers feature

--- tests/testhelper.h
+++ tests/testhelper.h
@@ -162,6 +162,7 @@ protected:
     // Finds \a layerName in the layer list view, verifies that it exists and that its name is \a layerName,
     // and returns the delegate for convenience.
     Q_REQUIRED_RESULT bool verifyLayerName(const QString &layerName, QQuickItem **layerDelegate = nullptr);
+    Q_REQUIRED_RESULT bool makeCurrentAndRenameLayer(const QString &from, const QString &to);
 
     void addAllProjectTypes();
     void addImageProjectTypes();
